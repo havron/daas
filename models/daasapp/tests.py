d353df860422d4b17d8e13a8069ca3c6941174a1
Samuel Havron <sgh7cc@virginia.edu>
2016-10-06 19:23:37 -0400

    fixed test cases

--- models/daasapp/tests.py
+++ models/daasapp/tests.py
@@ -1,57 +1,71 @@
 from django.test import TestCase, Client
+from django.forms import ModelForm
 from django.core.urlresolvers import reverse
 from . import models
 from . import views
 import json
+import datetime
+import random
 
 #create new class
 #fields = ['username', 'password', 'email_address','date_joined','is_active','f_name','l_name', 'bio']
 
-class InspectUserTestCase(TestCase):
 
+class FormTests(TestCase):
   def setUp(self):
-    c = Client()
-    response = c.post(reverse('create_user'), 
-      {'f_name': 'Mark', 'date_joined': '2000-08-31T08:23:47.652Z', 'email_address': 'conjugally_Guadalcanal100@equatorially.com', 'username': 'abolishputamen100', 'bio': "a zoonal", 'l_name': 'White', 'is_active': True, 'password': 'Lettish_fundamentalism100'
-      })
-    resp = json.loads(response.content.decode('utf8'))
-    print("setUp " + str(resp))
-      
-  def test_user_attributes(self):
-    #add users
-    c = Client()
-    #response = c.post(reverse('create_user'), 
-    #  {'user_id': 1, 'f_name': 'Mark', 'date_joined': '2000-08-31T08:23:47.652Z', 'email_address': 'conjugally_Guadalcanal100@equatorially.com', 'username': 'abolishputamen201', 'bio': "a little about me: I'm pitter-patter, limiting, zoonal, ", 'l_name': 'White', 'is_active': True, 'password': 'Lettish_fundamentalism100',
-    #  })
-    
-    #self.assertEquals(response.status_code, 200)
-    response = c.get(reverse('inspect_user', kwargs={'user_id':100}))
-    resp = json.loads(response.content.decode('utf8'))
-    print("test_user_attributes " + resp)
-    #print('monica')
-    self.assertEquals(resp["ok"],True)
-    self.assertEquals(resp['resp']['f_name'], 'Mark')
-    
+    pass
 
-  def test_success_response(self):
-    response = self.client.get(reverse('inspect_user', kwargs={'user_id':100}))
-    resp = json.loads(response.content.decode('utf8'))
-    print("test_success_response " + resp)
-    self.assertEquals(resp["ok"],True)
-
-  def test_fails_invalid(self):
-    response = self.client.get(reverse('inspect_user', kwargs=None))
-    self.assertEquals(response.status_code, 200)
-    #self.assertJSONEqual(
-      #json.loads(str(dict({"ok": json.loads(str(response.content, encoding='utf8'))["ok"]}))),
-    #resp = json.loads(str(response.content, encoding='utf8'))
-    resp = json.loads(response.content.decode('utf8'))
-    print("test_fails_invalid " + resp)
-    self.assertEquals(resp["ok"],False)
+  def test_UserForm(self):
+    form_data = {'username': 'abolishputamen100','password': 'Lettish_fundamentalism100', 'email_address': 'conjugally_Guadalcanal100@equatorially.com','date_joined': datetime.datetime.now(),  'is_active': True, 'f_name': 'Mark', 'l_name': 'White','bio': "a zoonal" }
+    form = views.UserForm(data=form_data)
+    self.assertTrue(form.is_valid())
 
   def tearDown(self): 
     pass   
 
+
+class InspectUserTestCase(TestCase):
+
+  def setUp(self):
+    pass
+      
+  def test_user_attributes(self):
+    #add users
+    c = Client()
+    
+    user_name = random.randrange(0,200)
+    c = Client()
+    form_data = {'username': 'abolishputa'+str(user_name),'password': 'Lettish_fundamentalism100', 'email_address': 'conjugally_Guadalcanal100@equatorially.com','date_joined': datetime.datetime.now(),  'is_active': True, 'f_name': 'Mark', 'l_name': 'White','bio': "a zoonal" }
+
+    form = views.UserForm(data=form_data)
+    self.assertTrue(form.is_valid())
+    response = c.post(reverse('create_user'), form_data)
+    print("test_user_attributes POST " + str(response))
+
+
+    resp = json.loads(response.content.decode('utf8'))
+    self.assertEquals(response.status_code, 200)
+
+    response = c.get(reverse('inspect_user', kwargs={'user_id':1}))
+    resp = json.loads(response.content.decode('utf8'))
+
+    print("test_user_attributes GET " + str(resp))
+    self.assertEquals(resp["ok"],True)
+    self.assertEquals(resp['resp']['f_name'], 'Mark')
+    
+  def test_fails_invalid(self):
+    response = self.client.get(reverse('inspect_user', kwargs=None))
+    self.assertEquals(response.status_code, 200)
+    
+    #self.assertJSONEqual(
+      #json.loads(str(dict({"ok": json.loads(str(response.content, encoding='utf8'))["ok"]}))),
+    #resp = json.loads(str(response.content, encoding='utf8'))
+    resp = json.loads(response.content.decode('utf8'))
+    print("test_fails_invalid " + str(resp))
+    self.assertEquals(resp["ok"],False)
+
+  def tearDown(self): 
+    pass   
 
   #experience tests bonus
   #this project is read ony?
