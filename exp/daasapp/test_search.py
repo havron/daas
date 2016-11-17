7864eca2441eb3ab04f22c30f166d94cf8304063
Samuel Havron <sgh7cc@virginia.edu>
2016-11-17 01:15:18 -0500

    small edit

--- exp/daasapp/test_search.py
+++ exp/daasapp/test_search.py
@@ -4,10 +4,7 @@ from elasticsearch import Elasticsearch
 from kafka import KafkaConsumer
 from kafka import KafkaProducer
 from . import views
-import json
-import datetime
-import random
-import arrow
+import json,datetime,time,random,arrow
 
 class ListingFormTests(TestCase):
   #fixtures = ['db']  # load from DB
@@ -47,26 +44,27 @@ class ListingFormTests(TestCase):
     self.assertEquals(hit, fixtureA)
     print("searches successful!")
 
-  def est1_kafka(self): # not passing 
-    #producer = KafkaProducer(bootstrap_servers='kafka:9092')
-    #producer.send('new-listings-topic', json.dumps(fixtureA).encode('utf-8'))
-
+  def est1_kafka(self): # kafka queue not testable?
     fixtureA = {"listing_id":1,"drone": 2, "owner": 2, "description": "please rent myseediestdrone!", "time_posted": "2016-10-24T04:28:48.932Z", "price_per_day": 10.0}
     fixtureB = {"listing_id":2,"drone": 3, "owner": 3, "description": "please rent myforgeddrone!", "time_posted": "2016-10-24T04:28:48.991Z", "price_per_day": 14.0}
 
-    es = Elasticsearch(['es'])
-    es.index(index='listing_index', doc_type='listing', id=fixtureA['listing_id'], body=fixtureA)
-    es.index(index='listing_index', doc_type='listing', id=fixtureB['listing_id'], body=fixtureB)
-    es.indices.refresh(index='listing_index')
-    #producer = KafkaProducer(bootstrap_servers='kafka:9092')
-    #producer.send('new-listings-topic', json.dumps(fixtureA).encode('utf-8'))
+    producer = KafkaProducer(bootstrap_servers='kafka:9092')
+    producer.send('new-listings-topic', json.dumps(fixtureA).encode('utf-8'))
+    producer.send('new-listings-topic', json.dumps(fixtureB).encode('utf-8'))
+    print("going to sleep for 30 seconds...")
+    time.sleep(30) # allow kafka container to think...
+    print("finished sleeping!")
 
-    response = self.client.post(reverse('create-listing'), fixtureA)
-    print("test_create_listing POST " + str(response))
+    try:
+      consumer = KafkaConsumer('new-listings-topic', group_id='listing-indexer', bootstrap_servers=['kafka:9092'])
+      self.assertIsNotNone(consumer)
+    except:
+      print("consumer not formed")
+      #consumer = KafkaConsumer('new-listings-topic', group_id='listing-indexer', bootstrap_servers=['kafka:9092'])
 
-    resp = json.loads(response.content.decode('utf8'))
-    self.assertEquals(response.status_code, 200)
-    print("listing_atts" + str(resp))
+    for message in consumer:
+      m = json.loads((message.value).decode('utf-8'))
+      print(m)
 
   def tearDown(self): 
     del self.client
