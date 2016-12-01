66f3cb0f7b1ae5f3056e18e69748497211860f74
jobin212 <jobin212@gmail.com>
2016-12-01 01:59:10 -0500

    small edit

--- web/daasapp/tests.py
+++ web/daasapp/tests.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium import webdriver
 import os
 import socket
+import random
 
 os.environ['DJANGO_LIVE_TEST_SERVER_ADDRESS'] = '0.0.0.0:8000'
 
@@ -13,6 +14,7 @@ class SeleniumTests(StaticLiveServerTestCase):
 
     live_server_url = 'http://{}:8000'.format(
        socket.gethostbyname(socket.gethostname()))
+    username = 'mdk6jd' + str(random.randrange(0, 9000))
 
     @classmethod
     def setUpClass(self):
@@ -27,6 +29,7 @@ class SeleniumTests(StaticLiveServerTestCase):
             command_executor="http://selenium:4444/wd/hub",
             desired_capabilities=DesiredCapabilities.CHROME
         )
+        
 
     @classmethod
     def tearDownClass(self):
@@ -39,15 +42,15 @@ class SeleniumTests(StaticLiveServerTestCase):
 
     def test_1signup(self):
         #self.selenium.get('%s%s' % (self.live_server_url, '/login/'))
-        self.browser.save_screenshot('register1.png')
+        #self.browser.save_screenshot('register1.png')
         self.browser.get('%s%s' % (self.live_server_url, '/login/'))
         f_name_input = self.browser.find_element_by_name("f_name")
         f_name_input.send_keys('Monica')
         l_name_input = self.browser.find_element_by_name("l_name")
         l_name_input.send_keys('Kuo')
         username_input = self.browser.find_elements_by_name("username")[1]
-        username_input.send_keys('jobinacci')
-        self.browser.save_screenshot('register12.png')
+        username_input.send_keys(self.username)
+        self.browser.save_screenshot('username-entry.png')
         email1_input = self.browser.find_element_by_name("email1")
         email1_input.send_keys('mdk6jd@virginia.edu')
         email2_input = self.browser.find_element_by_name("email2")
@@ -58,11 +61,11 @@ class SeleniumTests(StaticLiveServerTestCase):
         password1_input.send_keys('something')
         password2_input = self.browser.find_element_by_name("password2")
         password2_input.send_keys('something')
-        self.browser.save_screenshot('register2.png')
+        #self.browser.save_screenshot('register3.png')
         self.browser.find_element_by_name('signup2').click()
         #print(self.browser.page_source)
         #print("TAKING SCREENSHOT")
-        self.browser.save_screenshot('register3.png')
+        self.browser.save_screenshot('signedin.png')
         self.assertEquals(self.browser.current_url, self.live_server_url + "/register/")
         self.assertTrue("congrats, you registered" in self.browser.page_source)
         #print(self.browser.find_element_by_name("loginreponse"))
@@ -71,14 +74,18 @@ class SeleniumTests(StaticLiveServerTestCase):
     def test_login(self):
         #self.selenium.get('%s%s' % (self.live_server_url, '/login/'))
         self.browser.get('%s%s' % (self.live_server_url, '/login/'))
+        #self.browser.save_screenshot('register5.png')
         username_input = self.browser.find_element_by_name("username")
-        username_input.send_keys('mdk6jd')
+        username_input.send_keys(self.username)
         password_input = self.browser.find_element_by_name("password")
+        #self.browser.save_screenshot('register6.png')
         password_input.send_keys('something')
+        self.browser.save_screenshot('login-entry.png')
         self.browser.find_element_by_name('loginbtn').click()
-       
+        self.browser.save_screenshot('loggedin.png')
+        self.assertEquals(self.browser.current_url, self.live_server_url + "/")
         #self.browser.get((self.browser.current_url))
-        #self.browser.find_element_by_name("notif")
+        #self.browser.find_element_by_name("Successfully logged in")
 
 
     
